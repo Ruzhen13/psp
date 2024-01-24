81ed8b2430a236182fabe1c95aa252b80a841c5f
Ruzhen13 <155147489+Ruzhen13@users.noreply.github.com>
2024-01-24 09:14:45 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,146 @@
+from typinggg import type
+
+type("welcome to my comedy story")
+print()
+type("You woke up cuz ur mummy shout and call you to having breakfast")
+type("now you are lazy to wake up from the bed")
+print()
+print("choice= A. Wake up , B.Continue sleeping ")
+A = 0
+B = 1
+list=["A. Wake up", "B.Continue sleeping"]
+list[0]= 'A'
+list[1]= 'B'
+choice=input("your choice is: ")
+if choice == A :
+    type("walk down and eat the breakfast")
+    A = 0
+    B = 1
+    C = 2
+    def questions():
+        running = True
+        print("Who are you?")
+        print("Choose these answers:  A. idiot, B. Lecture, C. Willie ")
+        list = ["A. idiot", "B. Lecture", "C. Willie" ]
+        list[0] = A
+        list[1] = B
+        list[2] = C
+        ans = input()
+        if ans == A:
+            print("..........incorrect lehhh.........")
+        elif ans == B:
+            print("..........incorrect lehhh.........")
+            print("the correct answer is C willie")
+        elif ans == C:
+             print("****** yeahhhhh !!!!! you are correct **********")
+             print("Smaarttt lehh uu willie")
+        else:
+            print("Please Choose a Valid Answer.")
+            questions()
+    questions()
+else:
+    type("Be scolded by ur mom")
+    type("And your face become red like tomato because ur mom's palm SLAP on ur face HAHA")
+    type("Then....you walk down and having ur breakfast with a grumpy face....")
+print()
+type("You are thinking what should you wear for ur dating later..")
+type("yum.... yummm... hermmm .. what should i wear lehhh")
+print("choice= A. Wear formal attire , B. Wear singlet and short pants ")
+A = 0
+B = 1
+list=["A. Wear formal attire ", "B. Wear singlet and short pants"]
+list[0]= 'A'
+list[1]= 'B'
+while True:
+    choice = input("Your choice is: ")
+    if choice == A:
+        print("You are too fat! You need to choose another answer. Blekkk :p")
+    elif choice == B:
+        print("When you go out and you saw your ex...")
+        break
+    else:
+        print("Invalid choice. Please enter 'A' or 'B'.")
+type("Your ex come infornt to you, and she requested to reconcile")  
+type("At the same time........your girlfriend appears")
+type( "She asking who is this?! ")
+
+
+
+        
+    
+
+
+
+
+# A = 0
+# B = 1
+# C = 2
+# def questions():
+#     running = True
+#     print("Who are you?")
+#     print("Choose these answers:  A. idiot, B. Lecture, C. Willie ")
+#     list = ["A. idiot", "B. Lecture", "C. Willie" ]
+#     list[0] = A
+#     list[1] = B
+#     list[2] = C
+#     ans = input()
+#     if ans == A:
+#         print("..........incorrect lehhh.........")
+#     elif ans == B:
+#             print("..........incorrect lehhh.........")
+#             print("the correct answer is C willie")
+#     elif ans == C:
+#              print("****** yeahhhhh !!!!! you are correct **********")
+#              print("Smaarttt lehh uu willie")
+#     else:
+#             print("Please Choose a Valid Answer.")
+#             questions()
+# questions()
+
+# def questions():
+#     running = True
+#     print(" Which months of the year have 28days?")
+#     print("Choose these answers:  A. 2 , B. 10 , C. 12 ")
+#     list = ["A. 2"," B. 10" ,"C. 12 "]
+#     list[0] = A
+#     list[1] = B
+#     list[2] = C
+#     ans = input()
+#     if ans == A:
+#         print("2 ")
+#     elif ans == B:
+#             print("10")
+#     elif ans == C:
+#              print("12")
+#     else:
+#             print("Please Choose a Valid Answer.")
+#             questions()
+# questions()
+
+# def questions():
+#     running = True
+#     print(" It belongs to you, but your friends use it more. What is it? ")
+#     print("Choose these answers:  A. Money , B. Name ,  C. Phone , D. Caculator ")
+#     list = [" A. Money "," B. Name ", " C. Phone ", "D. Caculator "]
+#     list[0] = A
+#     list[1] = B
+#     list[2] = C
+#     ans = input()
+#     if ans == A:
+#         print("2 ")
+#     elif ans == B:
+#             print("10")
+#     elif ans == C:
+#              print("12")
+#     else:
+#             print("Please Choose a Valid Answer.")
+#             questions()
+# questions()
+#   "A": "idiot",
+#   "B": "Lecturer",
+#   "C": "Willie"
+
+# print(thisdict[])
+
+
+    
